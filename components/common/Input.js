0878a2fb4dd6c1f082a6817b88d42e9e2dd0bb5a
Stein, Jeff <jstein@ftdi.com>
2018-06-20 12:09:27 -0500

    enhance forms examples...
    ...create ButtonGroup comp. Add hint attr to inputs.  Make binary inputs work well.  Reorganize forms page.

--- components/common/Input.js
+++ components/common/Input.js
@@ -1,56 +1,129 @@
 import PropTypes from 'prop-types';
 
 import Label from './Label';
+import Button from './Button';
 
-function BinaryInput(props) {
-    return (<React.Fragment>
-        <legend>{props.children}</legend>
-        <div className='binary-group'>
-            { props.attrs.value.map((val, i) => {
-                return <div className='form-check' key={i}>
-                    <input className='form-check-input' type={props.type} name={props.attrs.id} id={props.attrs.id+'-'+i} value={val} checked={props.attrs.selected===i} />
-                    <Label className='form-check-label' htmlFor={props.attrs.id+'-'+i} text={val}></Label>
+class BinaryInput extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log('BinaryInput props: ',props);
+        this.state = {
+            // labelBinary:''
+            labelBinary: (props.attrs.selected===0||props.attrs.selected!=false) ? 'True' : 'False'
+        }
+    }
+
+    changeBinary(e) {
+        // console.log('START changeBinary: ', e.target.id);
+        // console.dir(e.target);
+        if (e.target.parentElement.parentElement.children.length === 1) {
+            // console.log('ONE CHILD!');
+            this.setState({
+                labelBinary: e.target.checked ? 'True' : 'False'
+            })
+        }
+    }
+    
+    render() {
+        const { type, attrs, children, hint } = this.props;
+        // console.log('RENDER attrs: ', attrs);
+        return (
+            <React.Fragment>
+                <legend>{children}</legend>
+                { hint &&
+                    <div><small class="form-text text-muted">{hint}</small></div>
+                }
+                <small><code>({attrs.id})</code></small>
+                <div className='binary-group'>
+                    { attrs.value.map((val, i) => {
+                        return <div className='form-check' key={i}>
+                            <input className='form-check-input' 
+                                defaultChecked={(attrs.selected===0||attrs.selected!=false) && attrs.selected===i} 
+                                onChange={this.changeBinary.bind(this)} 
+                                type={type} 
+                                name={attrs.id} 
+                                id={attrs.id+'-'+i} />
+                            <Label className='form-check-label' htmlFor={attrs.id+'-'+i} text={val ? val : this.state.labelBinary }></Label>
+                        </div>
+                    }) }
                 </div>
-            }) }
-        </div>
-    </React.Fragment>)
+            </React.Fragment>
+        )
+    }
 }
 
 function ButtonInput(props) {
-    return (<input className={`form-control btn btn-${props.attrs.flavor}`} type={props.type} name={props.attrs.id} id={props.attrs.id} defaultValue={props.attrs.value} />)
+    // an input element with type 'button', 'submit', or 'reset'
+    return (<input className={`form-control btn btn-${props.attrs.flavor}`} type={props.type} name={props.attrs.id} id={props.attrs.id} defaultValue={props.attrs.value && props.attrs.value} />)
 
 }
 
-class Input extends React.Component {
+function ButtonGroup(props) {
+    // array of button elements created with a single component invocation
+    return (
+        <div className='form-group input-group'>
+            { props.attrs.value.map((val, i) => {
+                // * input elements
+                // return (<ButtonInput key={i} type='button' attrs={{
+                //     flavor:props.attrs.flavor,
+                //     value: val,
+                //     id: props.attrs.id
+                // }}>{val}</ButtonInput>)
+                // * button elements
+                return (
+                    <Button key={i} attrs={{
+                        flavor: Array.isArray(props.attrs.flavor) ? props.attrs.flavor[i] : props.attrs.flavor,
+                        value: [val],
+                        id: props.attrs.id
+                    }}>{val}</Button>
+                )
+            }) }
+        </div>
+    )
+}
 
+class Input extends React.Component {
 	constructor(props) {
 		super(props);
+        console.log('Input props: ',props);
 		this.state = {
+            currentId: ''
 		}
-		// console.log('Input props: ',props);
     }
 
     changeHandler(e) {
         console.log(e.target.value);
-        
+        console.dir(e.target);
+        // const target = e.target;
+        // const value = target.type === 'checkbox' ? target.checked : target.value;
+        // const name = target.name;
+        // this.setState({
+        //   [name]: value
+        // });
     }
 
 	render() {
 
-        const { type, attrs, children } = this.props;
+        const { type, attrs, children, hint } = this.props;
         // const { value, name, id } = this.props.attrs;
         // console.log(type+': ',this.props);
 
         return (
-            <fieldset className='form-group'>
-                <p className='sticker'>{type} input</p>
+            <React.Fragment>
+            {/* <fieldset className='form-group'> */}
                 { (type === 'button' || type === 'reset' || type === 'submit') ?
                     <ButtonInput type={type} attrs={attrs}>{children}</ButtonInput>
                     : null
                 }
+                { type === 'button-group' &&
+                    <ButtonGroup attrs={attrs}></ButtonGroup>
+                }
                 { type === 'textarea' &&
                     <Label text={children}>
                         <textarea className='form-control' name={attrs.id} id={attrs.id} cols='30' rows='10' defaultValue={attrs.value} onChange={this.changeHandler.bind(this)}></textarea>
+                        { hint &&
+                            <div><small class="form-text text-muted">{hint}</small></div>
+                        }
                     </Label>
                 }
                 { type === 'select' &&
@@ -61,13 +134,17 @@ class Input extends React.Component {
                                 return <option key={attrs.id+'-'+i} value={val}>{val}</option>
                             }) }
                         </select>
+                        { hint &&
+                            <div><small class="form-text text-muted">{hint}</small></div>
+                        }
                     </Label>
                 }
                 { (type === 'radio' || type === 'checkbox') ?
-                    <BinaryInput type={type} attrs={attrs}>{children}</BinaryInput>
+                    <BinaryInput type={type} changeHandler={this.changeHandler.bind(this)} attrs={attrs} hint={hint}>{children}</BinaryInput>
                     : null
                 }
                 { type !== 'button' &&
+                    type !== 'button-group' &&
                     type !== 'reset' &&
                     type !== 'submit' &&
                     type !== 'checkbox' &&
@@ -75,7 +152,10 @@ class Input extends React.Component {
                     type !== 'textarea' &&
                     type !== 'select' &&
                     <Label text={children}>
-                        <input className='form-control' type={type} defaultValue={attrs.value[0]} onChange={this.changeHandler.bind(this)} />
+                        <input className='form-control' type={type} defaultValue={attrs.value && attrs.value[0]} onChange={this.changeHandler.bind(this)} placeholder={`${type?type:'undefined'} input`} />
+                        { hint &&
+                            <div><small class="form-text text-muted">{hint}</small></div>
+                        }
                     </Label>
                 }
                 <style jsx global>{`
@@ -84,14 +164,16 @@ class Input extends React.Component {
                     }
                     .sticker {
                         background: transparent;
-                        color:#099;
-                        font-weight:bold;
+                        color:#999;
+                        font-weight:normal;
                         font-size: 11px;
                         font-family: monospace;
-                        padding:0 1em;
+                        padding:0;
                         text-shadow:1px 1px 0 white, -1px 1px 0 white, 1px -1px 0 white, -1px -1px 0 white;
                         position:absolute;
-                        top:1.45em;
+                        // top:1.45em;
+                        top:-0.75em;
+                        left:0.5rem;
                     }
                     .binary-group {
                         border: 1px solid #ced4da;
@@ -100,18 +182,30 @@ class Input extends React.Component {
                         margin-bottom: .5rem;
                         display: block;
                     }
+                    input[type=color] {
+                        -webkit-appearance: none;
+                        padding:0.125rem;
+                        width:10rem;
+                        height:2rem;
+                    }
                 `}</style>
-            </fieldset>
+            {/* </fieldset> */}
+            </React.Fragment>
         );
 	}
 }
 
 Input.propTypes = {
-    type: PropTypes.string,
+    type: PropTypes.string.isRequired, // required
     attrs: PropTypes.shape({
-        value: PropTypes.array.isRequired,
-        id: PropTypes.string.isRequired,
-        selected: PropTypes.number
+        value: PropTypes.array.isRequired, // required
+        id: PropTypes.string.isRequired, // required
+        hint: PropTypes.string,
+        selected: PropTypes.number,
+        flavor:PropTypes.oneOfType([
+            PropTypes.string,
+            PropTypes.array
+        ])
     }),
 
 };
--- pages/forms.js
+++ pages/forms.js
@@ -12,65 +12,195 @@ const Forms = (props) => (
                 <p>{props.description}</p>
             </div>
             <div className='row'>
-                <div className="col-6">
-                    <div className=''>
+                <div className='col-4'>
+                    <div className='mx-2 my-4'>
+                        <h3>Form Inputs</h3>
                         <legend>Inputs</legend>
-                        <Input type='' attrs={{value:['jdsfgjdhfgh'], id:'input1'}}></Input>
-                        <Input type='text' attrs={{value:['jdsfgjdhfgh'], id:'text1'}}>Text</Input>
-                        <Input type='color' attrs={{value:['#66dd99'], id:'color1'}}>Color</Input>
-                        <Input type='email' attrs={{value:['jdfghg@kdjhfkgfhj.com'], id:'email1'}}>Email</Input>
-                        <Input type='datetime-local' attrs={{value:['2017-06-01T08:30'], id:'datetime1'}}>Date Time Local</Input>
-                        <Input type='number' attrs={{value:[84576], id:'number1'}}>Number</Input>
+                        <Input type='' 
+                            hint='This field has no type'
+                            attrs={{
+                                id:'input1'
+                            }}></Input>
+                        <Input type='text' 
+                            hint='This field is a text input'
+                            attrs={{
+                                value:[''], 
+                                id:'text1'
+                            }}>Text</Input>
+                        <Input type='password' 
+                            hint='Passwords must have 64 characters: 16 of [A-Za-z], 14 of [0-9], and 34 of [~!@#$%^*()_]'
+                            attrs={{
+                                value:['bdfhvjguiteu'], 
+                                id:'password1'
+                            }}>Password</Input>
+                        <Input type='color' 
+                            attrs={{
+                                value:['#66dd99'], 
+                                id:'color1'
+                            }}>Color</Input>
+                        <Input type='email' 
+                            attrs={{
+                                value:['jdfghg@kdjhfkgfhj.com'], 
+                                id:'email1'
+                            }}>Email</Input>
+                        <Input type='datetime-local' 
+                            attrs={{
+                                value:['2017-06-01T08:30'], 
+                                id:'datetime1'
+                            }}>Date Time Local</Input>
+                        <Input type='number' 
+                            attrs={{
+                                value:[84576], 
+                                id:'number1'
+                            }}>Number</Input>
                     </div>
-                    <div className=''>
+                    <div className='mx-2 my-4'>
                         <legend>Select/Option</legend>
-                        <Input type='select' attrs={{value:[
-                            'select/option jsdghkdf',
-                            'select/option ruiuherg',
-                            'select/option gfdhueur',
-                            'select/option fkgshidh'
-                        ], selected:2, id:'select1'}}>Select/Option</Input>
+                        <Input type='select' 
+                            attrs={{
+                                value:[
+                                    'select/option jsdghkdf',
+                                    'select/option ruiuherg',
+                                    'select/option gfdhueur',
+                                    'select/option fkgshidh'
+                                ], 
+                                selected:2, 
+                                id:'select1'
+                            }}>Select/Option</Input>
                     </div>
-                    <div className=''>
+                    <div className='mx-2 my-4'>
                         <legend>Textarea</legend>
-                        <Input type='textarea' attrs={{value:['Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque similique eaque aspernatur asperiores accusantium? Maxime quo at aliquid voluptas placeat ipsam libero a quam? Maiores excepturi maxime fugit explicabo perferendis?'], id:'textarea1'}}>Textarea</Input>
+                        <Input type='textarea' 
+                            attrs={{
+                                value:['Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque similique eaque aspernatur asperiores accusantium? Maxime quo at aliquid voluptas placeat ipsam libero a quam? Maiores excepturi maxime fugit explicabo perferendis?'],
+                                id:'textarea1'
+                            }}>Textarea</Input>
                     </div>
                 </div>
-                <div className="col-6">
-                    <div className=''>
+                <div className='col-4'>
+                    <div className='mx-2 my-4'>
+                        <h3>Binary Inputs</h3>
+                        <Input type='radio' 
+                            attrs={{
+                                value:[
+                                    'True',
+                                    'False'
+                                ], 
+                                selected:1, 
+                                id:'radios1'
+                            }}>Radio (true/false)</Input>
+                        <Input type='checkbox' 
+                            attrs={{
+                                value:[
+                                    ''
+                                ], 
+                                selected: 0, 
+                                id:'checkboxes1'
+                            }}
+                            hint='Toggle'>Checkbox (true/false)</Input>
+                        <Input type='radio' 
+                            attrs={{
+                                value:[
+                                    'Radio 1',
+                                    'Radio 2',
+                                    'Radio 3',
+                                    'Radio 4'
+                                ], 
+                                selected:2, 
+                                id:'radios2'
+                            }}
+                            hint='Pick one'>Radio (multi)</Input>
+                        <Input type='checkbox' 
+                            attrs={{
+                                value:[
+                                    'Checkbox 1',
+                                    'Checkbox 2',
+                                    'Checkbox 3',
+                                    'Checkbox 4'
+                                ], 
+                                selected:0, 
+                                id:'checkboxes2'
+                            }}
+                            hint='Pick many'>Checkbox (multi)</Input>
+                    </div>
+                </div>
+                <div className='col-4'>
+                    <div className='mx-2 my-4'>
+                        <h3>Buttons</h3>
                         <legend>Input Buttons</legend>
-                        <Input type='submit' attrs={{value:['Submit'], id:'button1', flavor:'dark'}} />
-                        <Input type='reset' attrs={{value:['Reset'], id:'button1', flavor:'primary'}} />
-                        <Input type='button' attrs={{value:['Button'], id:'button2', flavor:'secondary'}} />
-                    </div>
-                    <div className=''>
+                        <Input type='submit' 
+                            attrs={{
+                                value:['Submit'], 
+                                id:'button1', 
+                                flavor:'dark'
+                            }} />
+                        <Input type='reset' 
+                            attrs={{
+                                value:['Reset'], 
+                                id:'button2', 
+                                flavor:'primary'
+                            }} />
+                        <Input type='button' 
+                            attrs={{
+                                value:['Button'], 
+                                id:'button3', 
+                                flavor:'secondary'
+                            }} />
+                    {/* </div>
+                    <div className='mx-2 my-4'> */}
                         <legend>Buttons</legend>
-                        <Button type='button' attrs={{value:['jhfgdbhjdf'], id:'button1', flavor:'dark'}}>Button 1</Button>
-                        <Button type='button' attrs={{value:['dfgfbghjuf'], id:'button1', flavor:'primary'}}>Button 1</Button>
-                        <Button type='button' attrs={{value:['deruykgmnv'], id:'button2', flavor:'secondary'}}>Button 2</Button>
-                    </div>
-                    <div className=''>
+                        <Button type='button' 
+                            attrs={{
+                                value:['Button 1a'], 
+                                id:'button1a', 
+                                flavor:'dark'
+                            }}>Button 1a</Button>
+                        <Button type='button' 
+                            attrs={{
+                                value:['Button 2a'], 
+                                id:'button2a', 
+                                flavor:'primary'
+                            }}>Button 2a</Button>
+                        <Button type='button' 
+                            attrs={{
+                                value:['Button 3a'], 
+                                id:'button3a', 
+                                flavor:'secondary'
+                            }}>Button 3a</Button>
+                    {/* </div>
+                    <div className='mx-2 my-4'> */}
+                        <legend>ButtonGroup</legend>
+                        <Input type='button-group' 
+                            attrs={{
+                                value:[
+                                    'Button 1b',
+                                    'Button 2b',
+                                    'Button 3b'
+                                ], 
+                                id:'buttons-b', 
+                                // flavor:'secondary'
+                                flavor:['secondary','primary','dark']
+                            }}>Button Group</Input>
+                    {/* </div>
+                    <div className='mx-2 my-4'> */}
                         <legend>Buttons (group)</legend>
-                        <div className="input-group">
-                            <Button type='button' attrs={{value:['bvfhjsgodf'], id:'button-group1', flavor:'dark'}}>Button 1</Button>
-                            <Button type='button' attrs={{value:['jdsfgjdhfg'], id:'button-group1', flavor:'primary'}}>Button 1</Button>
-                            <Button type='button' attrs={{value:['fgweiruvwv'], id:'button-group2', flavor:'secondary'}}>Button 2</Button>
+                        <div className='input-group'>
+                            <Button type='button' 
+                                attrs={{
+                                    value:['Button 1c'], 
+                                    id:'button1c', flavor:'dark'
+                                }}>Button 1c</Button>
+                            <Button type='button' 
+                                attrs={{
+                                    value:['Button 2c'], 
+                                    id:'button2c', flavor:'primary'
+                                }}>Button 2c</Button>
+                            <Button type='button' 
+                                attrs={{
+                                    value:['Button 3c'], 
+                                    id:'button3c', flavor:'secondary'
+                                }}>Button 3c</Button>
                         </div>
-                    </div>
-                    <div className=''>
-                        <legend>Binary Inputs</legend>
-                        <Input type='checkbox' attrs={{value:[
-                            'Checkbox (JSD)',
-                            'Checkbox (RUI)',
-                            'Checkbox (GFD)',
-                            'Checkbox (FKG)'
-                        ], selected:1, id:'checkbox1'}}>Checkbox</Input>
-                        <Input type='radio' attrs={{value:[
-                            'Radio (JSD)',
-                            'Radio (RUI)',
-                            'Radio (GFD)',
-                            'Radio (FKG)'
-                        ], selected:2, id:'radio1'}}>Radio</Input>
                     </div>
                 </div>
             </div>
